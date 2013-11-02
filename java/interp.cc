7669e754ebc85ed4c2942ddbe0c1139915cb7e99
Pekka Enberg <penberg@kernel.org>
2013-11-02 14:46:03 +0200

    interp: Simplify with macros
    Use macros in the interpreter to consolidate code.
    
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- java/interp.cc
+++ java/interp.cc
@@ -27,6 +27,22 @@ value_t jint_to_value(jint n)
     return static_cast<value_t>(n);
 }
 
+#define CONST_INTERP(type, value)			\
+    do {						\
+        ostack.push(type##_to_value(value));		\
+    } while (0)
+
+#define LOAD_INTERP(type, idx)				\
+    do {						\
+        ostack.push(locals[idx]);			\
+    } while (0)
+
+#define STORE_INTERP(type, idx)				\
+    do {						\
+        locals[idx] = ostack.top();			\
+        ostack.pop();					\
+    } while (0)
+
 #define BINOP_INTERP(type, op)				\
     do {						\
         auto value2 = value_to_##type(ostack.top());	\
@@ -59,7 +75,7 @@ next_insn:
     case JVM_OPC_iconst_4:
     case JVM_OPC_iconst_5: {
         jint value = opc - JVM_OPC_iconst_0;
-        ostack.push(jint_to_value(value));
+        CONST_INTERP(jint, value);
         break;
     }
     case JVM_OPC_iload_0:
@@ -67,7 +83,7 @@ next_insn:
     case JVM_OPC_iload_2:
     case JVM_OPC_iload_3: {
         uint16_t idx = opc - JVM_OPC_iload_0;
-        ostack.push(locals[idx]);
+        LOAD_INTERP(jint, idx);
         break;
     }
     case JVM_OPC_aload_0:
@@ -75,13 +91,12 @@ next_insn:
     case JVM_OPC_aload_2:
     case JVM_OPC_aload_3: {
         uint16_t idx = opc - JVM_OPC_aload_0;
-        ostack.push(locals[idx]);
+        LOAD_INTERP(jobject, idx);
         break;
     }
     case JVM_OPC_istore: {
         auto idx = read_opc_u1(method->code + pc);
-        locals[idx] = ostack.top();
-        ostack.pop();
+        STORE_INTERP(jint, idx);
         break;
     }
     case JVM_OPC_istore_0:
@@ -89,8 +104,7 @@ next_insn:
     case JVM_OPC_istore_2:
     case JVM_OPC_istore_3: {
         uint16_t idx = opc - JVM_OPC_istore_0;
-        locals[idx] = ostack.top();
-        ostack.pop();
+        STORE_INTERP(jint, idx);
         break;
     }
     case JVM_OPC_pop: {
