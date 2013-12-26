a05357080c0bb57e7fdf5d625c3ec8f54a27ed0d
Pekka Enberg <penberg@kernel.org>
2013-12-26 17:34:09 +0000

    interp: if_icmpgt
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- java/interp.cc
+++ java/interp.cc
@@ -354,6 +354,10 @@ next_insn:
         IF_CMP_INTERP(jint, >=);
         break;
     }
+    case JVM_OPC_if_icmpgt: {
+        IF_CMP_INTERP(jint, >);
+        break;
+    }
     case JVM_OPC_goto: {
         int16_t offset = read_opc_u2(method->code + frame.pc);
         frame.pc += offset;
