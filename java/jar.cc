3af13da3f37c7a9585a96c13cac8b4c5f38607a0
Pekka Enberg <penberg@kernel.org>
2013-12-27 16:21:13 +0200

    jar: Fix loading of classes that do not exist
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- java/jar.cc
+++ java/jar.cc
@@ -20,8 +20,9 @@ jar::~jar()
 std::shared_ptr<klass> jar::load_class(std::string class_name)
 {
     zip_entry *entry = zip_entry_find_class(_zip, class_name.c_str());
-    assert(entry != nullptr);
-
+    if (!entry) {
+        return nullptr;
+    }
     char *data = (char*)zip_entry_data(_zip, entry);
 
     auto file = class_file{data, static_cast<size_t>(entry->uncomp_size)};
