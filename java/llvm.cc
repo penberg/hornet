3a2ba21378920265182f28e9503a28980fd6d85b
Pekka Enberg <penberg@kernel.org>
2014-04-28 21:18:52 +0300

    llvm: Simplify translator
    Replace 'llvm_context' with 'llvm_translator' and move the translation
    loop into it.
    
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- java/llvm.cc
+++ java/llvm.cc
@@ -21,23 +21,188 @@ namespace hornet {
 
 using namespace llvm;
 
-Module* module;
+enum class type {
+    t_int,
+};
+
+Module*          module;
 ExecutionEngine* engine;
 
-struct llvm_context {
-    llvm_context(Function* func_, IRBuilder<>& builder_,
-                 std::stack<Value*>& mimic_stack_, unsigned int max_locals)
-        : func(func_)
-        , builder(builder_)
-        , mimic_stack(mimic_stack_)
-        , locals(max_locals)
-    { }
+Type* typeof(type t)
+{
+    switch (t) {
+    case type::t_int: return Type::getInt32Ty(getGlobalContext());
+    default:          assert(0);
+    }
+}
 
-    Function*                func;
-    IRBuilder<>&             builder;
-    std::stack<Value*>&      mimic_stack;
-    std::vector<AllocaInst*> locals;
+Instruction::BinaryOps to_binary_op(binop op)
+{
+    switch (op) {
+    case binop::op_add: return Instruction::BinaryOps::Add;
+    default:            assert(0);
+    }
+}
+
+class llvm_translator {
+public:
+    llvm_translator(method* method);
+    ~llvm_translator();
+
+    void translate();
+    template<typename T>
+    T trampoline();
+    template<typename T>
+    void op_const     (type t, T value);
+    void op_load      (type t, uint16_t idx);
+    void op_store     (type t, uint16_t idx);
+    void op_binary    (type t, binop op);
+    void op_returnvoid();
+
+private:
+    AllocaInst* lookup_local(unsigned int idx, Type* type);
+
+    std::stack<Value*> _mimic_stack;
+    std::vector<AllocaInst*> _locals;
+    IRBuilder<> _builder;
+    Function* _func;
+    method* _method;
 };
+
+FunctionType* function_type(IRBuilder<>& builder, method* method)
+{
+    return FunctionType::get(builder.getVoidTy(), false);
+}
+
+Function* function(IRBuilder<>& builder, method* method)
+{
+    auto func_type = function_type(builder, method);
+    auto func = Function::Create(func_type, Function::ExternalLinkage, method->name, module);
+    auto entry = BasicBlock::Create(builder.getContext(), "entry", func);
+    builder.SetInsertPoint(entry);
+    return func;
+}
+
+llvm_translator::llvm_translator(method* method)
+    : _locals(method->max_locals)
+    , _builder(module->getContext())
+    , _method(method)
+{
+    _func = function(_builder, _method);
+}
+
+llvm_translator::~llvm_translator()
+{
+}
+
+void llvm_translator::translate()
+{
+    uint16_t pc = 0;
+
+next_insn:
+    assert(pc < _method->code_length);
+
+    uint8_t opc = _method->code[pc];
+
+    switch (opc) {
+    case JVM_OPC_iconst_m1:
+    case JVM_OPC_iconst_0:
+    case JVM_OPC_iconst_1:
+    case JVM_OPC_iconst_2:
+    case JVM_OPC_iconst_3:
+    case JVM_OPC_iconst_4:
+    case JVM_OPC_iconst_5: {
+        jint value = opc - JVM_OPC_iconst_0;
+        op_const<jint>(type::t_int, value);
+        break;
+    }
+    case JVM_OPC_iload_0:
+    case JVM_OPC_iload_1:
+    case JVM_OPC_iload_2:
+    case JVM_OPC_iload_3: {
+        uint16_t idx = opc - JVM_OPC_iload_0;
+        op_load(type::t_int, idx);
+        break;
+    }
+    case JVM_OPC_istore_0:
+    case JVM_OPC_istore_1:
+    case JVM_OPC_istore_2:
+    case JVM_OPC_istore_3: {
+        uint16_t idx = opc - JVM_OPC_istore_0;
+        op_store(type::t_int, idx);
+        break;
+    }
+    case JVM_OPC_iadd: {
+        op_binary(type::t_int, binop::op_add);
+        break;
+    }
+    case JVM_OPC_return:
+        op_returnvoid();
+        return;
+    default:
+        fprintf(stderr, "error: unsupported bytecode: %u\n", opc);
+        abort();
+    }
+
+    pc += opcode_length[opc];
+
+    goto next_insn;
+}
+
+template<typename T> T llvm_translator::trampoline()
+{
+    verifyFunction(*_func, PrintMessageAction);
+    return reinterpret_cast<T>(engine->getPointerToFunction(_func));
+}
+
+AllocaInst* llvm_translator::lookup_local(unsigned int idx, Type* type)
+{
+    if (_locals[idx]) {
+        return _locals[idx];
+    }
+    IRBuilder<> builder(&_func->getEntryBlock(), _func->getEntryBlock().begin());
+    auto ret = builder.CreateAlloca(type, nullptr, "");
+    _locals[idx] = ret;
+    return ret;
+}
+
+template<typename T>
+void llvm_translator::op_const(type t, T value)
+{
+    auto c = ConstantInt::get(typeof(t), value, 0);
+
+    _mimic_stack.push(c);
+}
+
+void llvm_translator::op_load(type t, uint16_t idx)
+{
+    auto value = _builder.CreateLoad(_locals[idx]);
+
+    _mimic_stack.push(value);
+}
+
+void llvm_translator::op_store(type t, uint16_t idx)
+{
+    auto value = _mimic_stack.top();
+    _mimic_stack.pop();
+    auto local = lookup_local(idx, typeof(t));
+    _builder.CreateStore(value, local);
+}
+
+void llvm_translator::op_binary(type t, binop op)
+{
+    auto value2 = _mimic_stack.top();
+    _mimic_stack.pop();
+    auto value1 = _mimic_stack.top();
+    _mimic_stack.pop();
+    auto result = _builder.CreateBinOp(to_binary_op(op), value1, value2);
+    _mimic_stack.push(result);
+}
+
+void llvm_translator::op_returnvoid()
+{
+    _builder.CreateRetVoid();
+}
 
 llvm_backend::llvm_backend()
 {
@@ -57,151 +222,14 @@ llvm_backend::~llvm_backend()
     delete engine;
 }
 
-FunctionType* function_type(IRBuilder<>& builder, method* method)
-{
-    return FunctionType::get(builder.getVoidTy(), false);
-}
-
-Function* function(IRBuilder<>& builder, method* method)
-{
-    auto func_type = function_type(builder, method);
-    auto func = Function::Create(func_type, Function::ExternalLinkage, method->name, module);
-    auto entry = BasicBlock::Create(builder.getContext(), "entry", func);
-    builder.SetInsertPoint(entry);
-    return func;
-}
-
-template<typename T> T trampoline(Function* func)
-{
-    return reinterpret_cast<T>(engine->getPointerToFunction(func));
-}
-
-static AllocaInst* lookup_local(llvm_context& ctx, unsigned int idx, Type* type)
-{
-    if (ctx.locals[idx]) {
-        return ctx.locals[idx];
-    }
-    IRBuilder<> builder(&ctx.func->getEntryBlock(), ctx.func->getEntryBlock().begin());
-    auto ret = builder.CreateAlloca(type, nullptr, "");
-    ctx.locals[idx] = ret;
-    return ret;
-}
-
-enum class type {
-    t_int,
-};
-
-Type* typeof(type t)
-{
-    switch (t) {
-    case type::t_int: return Type::getInt32Ty(getGlobalContext());
-    default:          assert(0);
-    }
-}
-
-template<typename T>
-void op_const(llvm_context& ctx, type t, T value)
-{
-    auto c = ConstantInt::get(typeof(t), value, 0);
-
-    ctx.mimic_stack.push(c);
-}
-
-void op_load(llvm_context& ctx, type t, uint16_t idx)
-{
-    auto value = ctx.builder.CreateLoad(ctx.locals[idx]);
-
-    ctx.mimic_stack.push(value);
-}
-
-void op_store(llvm_context& ctx, type t, uint16_t idx)
-{
-    auto value = ctx.mimic_stack.top();
-    ctx.mimic_stack.pop();
-    auto local = lookup_local(ctx, idx, typeof(t));
-    ctx.builder.CreateStore(value, local);
-}
-
-Instruction::BinaryOps to_binary_op(binop op)
-{
-    switch (op) {
-    case binop::op_add: return Instruction::BinaryOps::Add;
-    default:            assert(0);
-    }
-}
-
-void op_binary(llvm_context& ctx, type t, binop op)
-{
-    auto value2 = ctx.mimic_stack.top();
-    ctx.mimic_stack.pop();
-    auto value1 = ctx.mimic_stack.top();
-    ctx.mimic_stack.pop();
-    auto result = ctx.builder.CreateBinOp(to_binary_op(op), value1, value2);
-    ctx.mimic_stack.push(result);
-}
-
 value_t llvm_backend::execute(method* method, frame& frame)
 {
-    IRBuilder<> builder(module->getContext());
+    llvm_translator translator(method);
 
-    std::stack<llvm::Value*> mimic_stack;
+    translator.translate();
 
-    auto func = function(builder, method);
+    auto fp = translator.trampoline<value_t (*)()>();
 
-    llvm_context ctx(func, builder, mimic_stack, method->max_locals);
-
-next_insn:
-    assert(frame.pc < method->code_length);
-
-    uint8_t opc = method->code[frame.pc];
-
-    switch (opc) {
-    case JVM_OPC_iconst_m1:
-    case JVM_OPC_iconst_0:
-    case JVM_OPC_iconst_1:
-    case JVM_OPC_iconst_2:
-    case JVM_OPC_iconst_3:
-    case JVM_OPC_iconst_4:
-    case JVM_OPC_iconst_5: {
-        jint value = opc - JVM_OPC_iconst_0;
-        op_const<jint>(ctx, type::t_int, value);
-        break;
-    }
-    case JVM_OPC_iload_0:
-    case JVM_OPC_iload_1:
-    case JVM_OPC_iload_2:
-    case JVM_OPC_iload_3: {
-        uint16_t idx = opc - JVM_OPC_iload_0;
-        op_load(ctx, type::t_int, idx);
-        break;
-    }
-    case JVM_OPC_istore_0:
-    case JVM_OPC_istore_1:
-    case JVM_OPC_istore_2:
-    case JVM_OPC_istore_3: {
-        uint16_t idx = opc - JVM_OPC_istore_0;
-        op_store(ctx, type::t_int, idx);
-        break;
-    }
-    case JVM_OPC_iadd: {
-        op_binary(ctx, type::t_int, binop::op_add);
-        break;
-    }
-    case JVM_OPC_return:
-        builder.CreateRetVoid();
-        goto exit;
-    default:
-        fprintf(stderr, "error: unsupported bytecode: %u\n", opc);
-        abort();
-    }
-
-    frame.pc += opcode_length[opc];
-
-    goto next_insn;
-exit:
-    verifyFunction(*func, PrintMessageAction);
-
-    auto fp = trampoline<value_t (*)()>(func);
     return fp();
 }
 
