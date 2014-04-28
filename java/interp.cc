e81066d16c90edc51efb4ca95691f3249da45ce1
Pekka Enberg <penberg@kernel.org>
2014-04-28 19:38:53 +0300

    interp: Generify translator code
    This makes the core translator loop more generic so that we hopefully
    can share it the same loop between the interpreter and LLVM and DynASM
    backends.
    
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- java/interp.cc
+++ java/interp.cc
@@ -35,169 +35,232 @@ array* from_value<array*>(value_t value)
 }
 
 template<typename T>
-void const_interp(frame& frame, T value)
+void op_const(frame& frame, T value)
 {
-    frame.ostack.push(to_value(value));
+    frame.ostack.push(to_value<T>(value));
 }
 
-void load_interp(frame& frame, uint16_t idx)
+void op_load(frame& frame, uint16_t idx)
 {
     frame.ostack.push(frame.locals[idx]);
 }
 
-void store_interp(frame& frame, uint16_t idx)
+void op_store(frame& frame, uint16_t idx)
 {
     frame.locals[idx] = frame.ostack.top();
     frame.ostack.pop();
 }
 
-template<typename T>
-void unop_interp(frame& frame, std::function<T (T)> op)
+void op_pop(frame& frame)
 {
-    auto value = from_value<T>(frame.ostack.top());
     frame.ostack.pop();
-    auto result = op(value);
-    frame.ostack.push(to_value<T>(result));
+}
+
+void op_dup(frame& frame)
+{
+    auto value = frame.ostack.top();
+    frame.ostack.push(value);
+}
+
+void op_dup_x1(frame& frame)
+{
+    auto value1 = frame.ostack.top();
+    frame.ostack.pop();
+    auto value2 = frame.ostack.top();
+    frame.ostack.pop();
+
+    frame.ostack.push(value1);
+    frame.ostack.push(value2);
+    frame.ostack.push(value1);
+}
+
+void op_swap(frame& frame)
+{
+    auto value1 = frame.ostack.top();
+    frame.ostack.pop();
+    auto value2 = frame.ostack.top();
+    frame.ostack.pop();
+    frame.ostack.push(value1);
+    frame.ostack.push(value2);
+}
+
+enum class unop {
+    op_neg,
+};
+
+template<typename T>
+T eval(unop op, T a)
+{
+    switch (op) {
+    case unop::op_neg: return -a;
+    default:           assert(0);
+    }
 }
 
 template<typename T>
-void binop_interp(frame& frame, std::function<T (T, T)> op)
+void op_unary(frame& frame, unop op)
+{
+    auto value = from_value<T>(frame.ostack.top());
+    frame.ostack.pop();
+    auto result = eval(op, value);
+    frame.ostack.push(to_value<T>(result));
+}
+
+enum class binop {
+    op_add,
+    op_sub,
+    op_mul,
+    op_div,
+    op_rem,
+    op_and,
+    op_or,
+    op_xor,
+};
+
+template<typename T>
+T eval(binop op, T a, T b)
+{
+    switch (op) {
+    case binop::op_add: return a + b;
+    case binop::op_sub: return a - b;
+    case binop::op_mul: return a * b;
+    case binop::op_div: return a / b;
+    case binop::op_rem: return a % b;
+    case binop::op_and: return a & b;
+    case binop::op_or:  return a | b;
+    case binop::op_xor: return a ^ b;
+    default:            assert(0);
+    }
+}
+
+template<>
+jfloat eval(binop op, jfloat a, jfloat b)
+{
+    switch (op) {
+    case binop::op_add: return a + b;
+    case binop::op_sub: return a - b;
+    case binop::op_mul: return a * b;
+    case binop::op_div: return a / b;
+    default:            assert(0);
+    }
+}
+
+template<>
+jdouble eval(binop op, jdouble a, jdouble b)
+{
+    switch (op) {
+    case binop::op_add: return a + b;
+    case binop::op_sub: return a - b;
+    case binop::op_mul: return a * b;
+    case binop::op_div: return a / b;
+    default:            assert(0);
+    }
+}
+
+template<typename T>
+void op_binary(frame& frame, binop op)
 {
     auto value2 = from_value<T>(frame.ostack.top());
     frame.ostack.pop();
     auto value1 = from_value<T>(frame.ostack.top());
     frame.ostack.pop();
-    auto result = op(value1, value2);
+    auto result = eval(op, value1, value2);
     frame.ostack.push(to_value<T>(result));
 }
 
+void op_iinc(frame& frame, uint16_t idx, jint value)
+{
+    frame.locals[idx] += value;
+}
+
+enum class shiftop {
+    op_shl,
+    op_shr,
+};
+
 template<typename T>
-void shift_interp(frame& frame, std::function<T (T, jint)> op, jint mask)
+T eval(shiftop op, T a, jint b)
+{
+    switch (op) {
+    case shiftop::op_shl: return a << b;
+    case shiftop::op_shr: return a >> b;
+    default:              assert(0);
+    }
+}
+
+template<typename T>
+void op_shift(frame& frame, shiftop op, jint mask)
 {
     auto value2 = from_value<jint>(frame.ostack.top());
     frame.ostack.pop();
     auto value1 = from_value<T>(frame.ostack.top());
     frame.ostack.pop();
-    auto result = op(value1, value2 & mask);
+    auto result = eval(op, value1, value2 & mask);
     frame.ostack.push(to_value<T>(result));
 }
 
+enum class cmpop {
+    op_cmpeq,
+    op_cmpne,
+    op_cmplt,
+    op_cmpge,
+    op_cmpgt,
+    op_cmple,
+};
+
 template<typename T>
-void if_cmp_interp(method* method, frame& frame, std::function<bool (T, T)> op)
+bool eval(cmpop op, T a, T b)
+{
+    switch (op) {
+    case cmpop::op_cmpeq: return a == b;
+    case cmpop::op_cmpne: return a != b;
+    case cmpop::op_cmplt: return a < b;
+    case cmpop::op_cmpge: return a >= b;
+    case cmpop::op_cmpgt: return a > b;
+    case cmpop::op_cmple: return a <= b;
+    default:              assert(0);
+    }
+}
+
+template<typename T>
+void op_if_cmp(method* method, frame& frame, cmpop op, int16_t offset)
 {
     uint8_t opc = method->code[frame.pc];
-    int16_t offset = read_opc_u2(method->code + frame.pc);
     auto value2 = from_value<T>(frame.ostack.top());
     frame.ostack.pop();
     auto value1 = from_value<T>(frame.ostack.top());
     frame.ostack.pop();
-    if (op(value1, value2)) {
+    if (eval(op, value1, value2)) {
         frame.pc += offset;
     } else {
         frame.pc += opcode_length[opc];
     }
 }
 
-template<typename T>
-T op_add(T a, T b)
+void op_goto(frame& frame, int16_t offset)
 {
-    return a + b;
+    frame.pc += offset;
 }
 
-template<typename T>
-T op_sub(T a, T b)
+void op_getstatic(method* method, frame& frame, uint16_t idx)
 {
-    return a - b;
+    auto field = method->klass->resolve_field(idx);
+    assert(field != nullptr);
+    frame.ostack.push(field->value);
 }
 
-template<typename T>
-T op_mul(T a, T b)
+void op_new(frame& frame)
 {
-    return a * b;
+    auto obj = gc_new_object(nullptr);
+    frame.ostack.push(to_value<object*>(obj));
 }
 
-template<typename T>
-T op_div(T a, T b)
+void op_arraylength(frame& frame)
 {
-    return a / b;
-}
-
-template<typename T>
-T op_rem(T a, T b)
-{
-    return a % b;
-}
-
-template<typename T>
-T op_and(T a, T b)
-{
-    return a & b;
-}
-
-template<typename T>
-T op_or(T a, T b)
-{
-    return a | b;
-}
-
-template<typename T>
-T op_xor(T a, T b)
-{
-    return a ^ b;
-}
-
-template<typename T>
-T op_neg(T a)
-{
-    return -a;
-}
-
-template<typename T>
-T op_shl(T a, T b)
-{
-    return a << b;
-}
-
-template<typename T>
-T op_shr(T a, T b)
-{
-    return a >> b;
-}
-
-template<typename T>
-bool op_cmpeq(T a, T b)
-{
-    return a == b;
-}
-
-template<typename T>
-bool op_cmpne(T a, T b)
-{
-    return a != b;
-}
-
-template<typename T>
-bool op_cmplt(T a, T b)
-{
-    return a < b;
-}
-
-template<typename T>
-bool op_cmpge(T a, T b)
-{
-    return a >= b;
-}
-
-template<typename T>
-bool op_cmpgt(T a, T b)
-{
-    return a > b;
-}
-
-template<typename T>
-bool op_cmple(T a, T b)
-{
-    return a <= b;
+    auto* arrayref = from_value<array*>(frame.ostack.top());
+    frame.ostack.pop();
+    assert(arrayref != nullptr);
+    frame.ostack.push(arrayref->length);
 }
 
 value_t interp_backend::execute(method* method, frame& frame)
@@ -211,7 +274,7 @@ next_insn:
     case JVM_OPC_nop:
         break;
     case JVM_OPC_aconst_null:
-        frame.ostack.push(to_value<jobject>(nullptr));
+        op_const<object*>(frame, nullptr);
         break;
     case JVM_OPC_iconst_m1:
     case JVM_OPC_iconst_0:
@@ -221,35 +284,35 @@ next_insn:
     case JVM_OPC_iconst_4:
     case JVM_OPC_iconst_5: {
         jint value = opc - JVM_OPC_iconst_0;
-        const_interp(frame, value);
+        op_const(frame, value);
         break;
     }
     case JVM_OPC_lconst_0:
     case JVM_OPC_lconst_1: {
         jlong value = opc - JVM_OPC_lconst_0;
-        const_interp(frame, value);
+        op_const(frame, value);
         break;
     }
     case JVM_OPC_fconst_0:
     case JVM_OPC_fconst_1: {
         jfloat value = opc - JVM_OPC_fconst_0;
-        const_interp(frame, value);
+        op_const(frame, value);
         break;
     }
     case JVM_OPC_dconst_0:
     case JVM_OPC_dconst_1: {
         jdouble value = opc - JVM_OPC_dconst_0;
-        const_interp(frame, value);
+        op_const(frame, value);
         break;
     }
     case JVM_OPC_bipush: {
         int8_t value = read_opc_u1(method->code + frame.pc);
-        const_interp<jint>(frame, value);
+        op_const<jint>(frame, value);
         break;
     }
     case JVM_OPC_sipush: {
         int16_t value = read_opc_u2(method->code + frame.pc);
-        const_interp<jint>(frame, value);
+        op_const<jint>(frame, value);
         break;
     }
     case JVM_OPC_ldc: {
@@ -259,7 +322,7 @@ next_insn:
         switch (cp_info->tag) {
         case cp_tag::const_integer: {
             auto value = const_pool->get_integer(idx);
-            frame.ostack.push(to_value<jint>(value));
+            op_const<jint>(frame, value);
             break;
         }
         default:
@@ -270,17 +333,17 @@ next_insn:
     }
     case JVM_OPC_iload: {
         auto idx = read_opc_u1(method->code + frame.pc);
-        load_interp(frame, idx);
+        op_load(frame, idx);
         break;
     }
     case JVM_OPC_lload: {
         auto idx = read_opc_u1(method->code + frame.pc);
-        load_interp(frame, idx);
+        op_load(frame, idx);
         break;
     }
     case JVM_OPC_aload: {
         auto idx = read_opc_u1(method->code + frame.pc);
-        load_interp(frame, idx);
+        op_load(frame, idx);
         break;
     }
     case JVM_OPC_iload_0:
@@ -288,7 +351,7 @@ next_insn:
     case JVM_OPC_iload_2:
     case JVM_OPC_iload_3: {
         uint16_t idx = opc - JVM_OPC_iload_0;
-        load_interp(frame, idx);
+        op_load(frame, idx);
         break;
     }
     case JVM_OPC_lload_0:
@@ -296,7 +359,7 @@ next_insn:
     case JVM_OPC_lload_2:
     case JVM_OPC_lload_3: {
         uint16_t idx = opc - JVM_OPC_lload_0;
-        load_interp(frame, idx);
+        op_load(frame, idx);
         break;
     }
     case JVM_OPC_aload_0:
@@ -304,17 +367,17 @@ next_insn:
     case JVM_OPC_aload_2:
     case JVM_OPC_aload_3: {
         uint16_t idx = opc - JVM_OPC_aload_0;
-        load_interp(frame, idx);
+        op_load(frame, idx);
         break;
     }
     case JVM_OPC_istore: {
         auto idx = read_opc_u1(method->code + frame.pc);
-        store_interp(frame, idx);
+        op_store(frame, idx);
         break;
     }
     case JVM_OPC_lstore: {
         auto idx = read_opc_u1(method->code + frame.pc);
-        store_interp(frame, idx);
+        op_store(frame, idx);
         break;
     }
     case JVM_OPC_istore_0:
@@ -322,7 +385,7 @@ next_insn:
     case JVM_OPC_istore_2:
     case JVM_OPC_istore_3: {
         uint16_t idx = opc - JVM_OPC_istore_0;
-        store_interp(frame, idx);
+        op_store(frame, idx);
         break;
     }
     case JVM_OPC_lstore_0:
@@ -330,200 +393,192 @@ next_insn:
     case JVM_OPC_lstore_2:
     case JVM_OPC_lstore_3: {
         uint16_t idx = opc - JVM_OPC_lstore_0;
-        store_interp(frame, idx);
+        op_store(frame, idx);
         break;
     }
     case JVM_OPC_pop: {
-        frame.ostack.pop();
+        op_pop(frame);
         break;
     }
     case JVM_OPC_dup: {
-        auto value = frame.ostack.top();
-        frame.ostack.push(value);
+        op_dup(frame);
         break;
     }
     case JVM_OPC_dup_x1: {
-        auto value1 = frame.ostack.top();
-        frame.ostack.pop();
-        auto value2 = frame.ostack.top();
-        frame.ostack.pop();
-
-        frame.ostack.push(value1);
-        frame.ostack.push(value2);
-        frame.ostack.push(value1);
+        op_dup_x1(frame);
         break;
     }
     case JVM_OPC_swap: {
-        auto value1 = frame.ostack.top();
-        frame.ostack.pop();
-        auto value2 = frame.ostack.top();
-        frame.ostack.pop();
-
-        frame.ostack.push(value1);
-        frame.ostack.push(value2);
+        op_swap(frame);
         break;
     }
     case JVM_OPC_iadd: {
-        binop_interp<jint>(frame, op_add<jint>);
+        op_binary<jint>(frame, binop::op_add);
         break;
     }
     case JVM_OPC_ladd: {
-        binop_interp<jlong>(frame, op_add<jlong>);
+        op_binary<jlong>(frame, binop::op_add);
         break;
     }
     case JVM_OPC_fadd: {
-        binop_interp<jfloat>(frame, op_add<jfloat>);
+        op_binary<jfloat>(frame, binop::op_add);
         break;
     }
     case JVM_OPC_dadd: {
-        binop_interp<jdouble>(frame, op_add<jdouble>);
+        op_binary<jdouble>(frame, binop::op_add);
         break;
     }
     case JVM_OPC_isub: {
-        binop_interp<jint>(frame, op_sub<jint>);
+        op_binary<jint>(frame, binop::op_sub);
         break;
     }
     case JVM_OPC_lsub: {
-        binop_interp<jlong>(frame, op_sub<jlong>);
+        op_binary<jlong>(frame, binop::op_sub);
         break;
     }
     case JVM_OPC_fsub: {
-        binop_interp<jfloat>(frame, op_sub<jfloat>);
+        op_binary<jfloat>(frame, binop::op_sub);
         break;
     }
     case JVM_OPC_dsub: {
-        binop_interp<jdouble>(frame, op_sub<jdouble>);
+        op_binary<jdouble>(frame, binop::op_sub);
         break;
     }
     case JVM_OPC_imul: {
-        binop_interp<jint>(frame, op_mul<jint>);
+        op_binary<jint>(frame, binop::op_mul);
         break;
     }
     case JVM_OPC_lmul: {
-        binop_interp<jlong>(frame, op_mul<jlong>);
+        op_binary<jlong>(frame, binop::op_mul);
         break;
     }
     case JVM_OPC_fmul: {
-        binop_interp<jfloat>(frame, op_mul<jfloat>);
+        op_binary<jfloat>(frame, binop::op_mul);
         break;
     }
     case JVM_OPC_dmul: {
-        binop_interp<jdouble>(frame, op_mul<jdouble>);
+        op_binary<jdouble>(frame, binop::op_mul);
         break;
     }
     case JVM_OPC_idiv: {
-        binop_interp<jint>(frame, op_div<jint>);
+        op_binary<jint>(frame, binop::op_div);
         break;
     }
     case JVM_OPC_ldiv: {
-        binop_interp<jlong>(frame, op_div<jlong>);
+        op_binary<jlong>(frame, binop::op_div);
         break;
     }
     case JVM_OPC_fdiv: {
-        binop_interp<jfloat>(frame, op_div<jfloat>);
+        op_binary<jfloat>(frame, binop::op_div);
         break;
     }
     case JVM_OPC_ddiv: {
-        binop_interp<jdouble>(frame, op_div<jdouble>);
+        op_binary<jdouble>(frame, binop::op_div);
         break;
     }
     case JVM_OPC_irem: {
-        binop_interp<jint>(frame, op_rem<jint>);
+        op_binary<jint>(frame, binop::op_rem);
         break;
     }
     case JVM_OPC_lrem: {
-        binop_interp<jlong>(frame, op_rem<jlong>);
+        op_binary<jlong>(frame, binop::op_rem);
         break;
     }
     case JVM_OPC_ineg: {
-        unop_interp<jint>(frame, op_neg<jint>);
+        op_unary<jint>(frame, unop::op_neg);
         break;
     }
     case JVM_OPC_lneg: {
-        unop_interp<jlong>(frame, op_neg<jlong>);
+        op_unary<jlong>(frame, unop::op_neg);
         break;
     }
     case JVM_OPC_fneg: {
-        unop_interp<jfloat>(frame, op_neg<jfloat>);
+        op_unary<jfloat>(frame, unop::op_neg);
         break;
     }
     case JVM_OPC_dneg: {
-        unop_interp<jdouble>(frame, op_neg<jdouble>);
+        op_unary<jdouble>(frame, unop::op_neg);
         break;
     }
     case JVM_OPC_ishl: {
-        shift_interp<jint>(frame, op_shl<jint>, 0x1f);
+        op_shift<jint>(frame, shiftop::op_shl, 0x1f);
         break;
     }
     case JVM_OPC_lshl: {
-        shift_interp<jlong>(frame, op_shl<jlong>, 0x3f);
+        op_shift<jlong>(frame, shiftop::op_shl, 0x3f);
         break;
     }
     case JVM_OPC_ishr: {
-        shift_interp<jint>(frame, op_shr<jint>, 0x1f);
+        op_shift<jint>(frame, shiftop::op_shr, 0x1f);
         break;
     }
     case JVM_OPC_lshr: {
-        shift_interp<jlong>(frame, op_shr<jlong>, 0x3f);
+        op_shift<jlong>(frame, shiftop::op_shr, 0x3f);
         break;
     }
     case JVM_OPC_iand: {
-        binop_interp<jint>(frame, op_and<jint>);
+        op_binary<jint>(frame, binop::op_and);
         break;
     }
     case JVM_OPC_land: {
-        binop_interp<jlong>(frame, op_and<jlong>);
+        op_binary<jlong>(frame, binop::op_and);
         break;
     }
     case JVM_OPC_ior: {
-        binop_interp<jint>(frame, op_or<jint>);
+        op_binary<jint>(frame, binop::op_or);
         break;
     }
     case JVM_OPC_lor: {
-        binop_interp<jlong>(frame, op_or<jlong>);
+        op_binary<jlong>(frame, binop::op_or);
         break;
     }
     case JVM_OPC_ixor: {
-        binop_interp<jint>(frame, op_xor<jint>);
+        op_binary<jint>(frame, binop::op_xor);
         break;
     }
     case JVM_OPC_lxor: {
-        binop_interp<jlong>(frame, op_xor<jlong>);
+        op_binary<jlong>(frame, binop::op_xor);
         break;
     }
     case JVM_OPC_iinc: {
-        auto idx = read_opc_u1(method->code + frame.pc);
-        auto c   = read_opc_u1(method->code + frame.pc + 1);
-        frame.locals[idx] += c;
+        auto idx   = read_opc_u1(method->code + frame.pc);
+        auto value = read_opc_u1(method->code + frame.pc + 1);
+        op_iinc(frame, idx, value);
         break;
     }
     case JVM_OPC_if_icmpeq: {
-        if_cmp_interp<jint>(method, frame, op_cmpeq<jint>);
+        int16_t offset = read_opc_u2(method->code + frame.pc);
+        op_if_cmp<jint>(method, frame, cmpop::op_cmpeq, offset);
         goto next_insn;
     }
     case JVM_OPC_if_icmpne: {
-        if_cmp_interp<jint>(method, frame, op_cmpne<jint>);
+        int16_t offset = read_opc_u2(method->code + frame.pc);
+        op_if_cmp<jint>(method, frame, cmpop::op_cmpne, offset);
         goto next_insn;
     }
     case JVM_OPC_if_icmplt: {
-        if_cmp_interp<jint>(method, frame, op_cmplt<jint>);
+        int16_t offset = read_opc_u2(method->code + frame.pc);
+        op_if_cmp<jint>(method, frame, cmpop::op_cmplt, offset);
         goto next_insn;
     }
     case JVM_OPC_if_icmpge: {
-        if_cmp_interp<jint>(method, frame, op_cmpge<jint>);
+        int16_t offset = read_opc_u2(method->code + frame.pc);
+        op_if_cmp<jint>(method, frame, cmpop::op_cmpge, offset);
         goto next_insn;
     }
     case JVM_OPC_if_icmpgt: {
-        if_cmp_interp<jint>(method, frame, op_cmpgt<jint>);
+        int16_t offset = read_opc_u2(method->code + frame.pc);
+        op_if_cmp<jint>(method, frame, cmpop::op_cmpgt, offset);
         goto next_insn;
     }
     case JVM_OPC_if_icmple: {
-        if_cmp_interp<jint>(method, frame, op_cmple<jint>);
+        int16_t offset = read_opc_u2(method->code + frame.pc);
+        op_if_cmp<jint>(method, frame, cmpop::op_cmple, offset);
         goto next_insn;
     }
     case JVM_OPC_goto: {
         int16_t offset = read_opc_u2(method->code + frame.pc);
-        frame.pc += offset;
+        op_goto(frame, offset);
         goto next_insn;
     }
     case JVM_OPC_ireturn:
@@ -541,9 +596,7 @@ next_insn:
     }
     case JVM_OPC_getstatic: {
         uint16_t idx = read_opc_u2(method->code + frame.pc);
-        auto field = method->klass->resolve_field(idx);
-        assert(field != nullptr);
-        frame.ostack.push(field->value);
+        op_getstatic(method, frame, idx);
     }
     case JVM_OPC_invokespecial: {
         break;
@@ -566,15 +619,11 @@ next_insn:
         break;
     }
     case JVM_OPC_new: {
-        auto obj = gc_new_object(nullptr);
-        frame.ostack.push(to_value<object*>(obj));
+        op_new(frame);
         break;
     }
     case JVM_OPC_arraylength: {
-        auto* arrayref = from_value<array*>(frame.ostack.top());
-        frame.ostack.pop();
-        assert(arrayref != nullptr);
-        frame.ostack.push(arrayref->length);
+        op_arraylength(frame);
         break;
     }
     default:
