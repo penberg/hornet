29186425bc2c4e7a823e9d81bdb62d45fe3297e0
Pekka Enberg <penberg@kernel.org>
2014-04-29 21:07:06 +0300

    java/translator: OPC_nop
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- java/translator.cc
+++ java/translator.cc
@@ -24,6 +24,8 @@ next_insn:
     uint8_t opc = _method->code[pc];
 
     switch (opc) {
+    case JVM_OPC_nop:
+        break;
     case JVM_OPC_iconst_m1:
     case JVM_OPC_iconst_0:
     case JVM_OPC_iconst_1:
