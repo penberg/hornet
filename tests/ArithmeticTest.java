cd7f4c5711d7fb174cec00bd7fbb3569d736fecf
Pekka Enberg <penberg@kernel.org>
2014-04-29 19:46:07 +0300

    tests: Integer arithmetic
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- tests/ArithmeticTest.java
+++ tests/ArithmeticTest.java
@@ -2,6 +2,14 @@ public class ArithmeticTest {
   public static void main(String[] args) {
     int i = 1;
     int j = 2;
-    int k = i + j;
+    int k;
+    k = i + j;
+    k = i - j;
+    k = i * j;
+    k = i / j;
+    k = i % j;
+    k = i & j;
+    k = i | j;
+    k = i ^ j;
   }
 }
