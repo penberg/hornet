penberg/hornet

commit a9af8d0c7a58c92ffac96e1d03111e82adcbdc4a

Makefile: add -ltinfo and -ldl to libs
Needed on Ubuntu

Signed-off-by: Jiao Xiang <jxwr.cn@gmail.com>

Makefile
@@ -110,7 +110,7 @@ all: $(PROGRAMS)
 
 hornet: $(OBJS) hornet.cc
 	$(E) "  LINK  " $@
-	$(Q) $(CXX) $(CXXFLAGS) $(OBJS) $(LDFLAGS) $(LIBS) -lz hornet.cc -o hornet
+	$(Q) $(CXX) $(CXXFLAGS) $(OBJS) $(LDFLAGS) $(LIBS) -ltinfo -ldl -lz hornet.cc -o hornet
 
 define INSTALL_EXEC
 	install -v $1 $(DESTDIR)$2/$1 || exit 1;

commit 0897560ee9089b7477b544f6741ac61e78e4024d

Makefile: Fix linking on Darwin
Fix Darwin linkage fallout from commit a9af8d0 ("Makefile: add -ltinfo
and -ldl to libs").

Signed-off-by: Pekka Enberg <penberg@iki.fi>

Makefile
@@ -68,6 +68,7 @@ ifeq ($(uname_S),Darwin)
 	CONFIGURATIONS += -DCONFIG_NEED_MAP_ANONYMOUS
 else
 	INCLUDES += -I$(JAVA_HOME)/include/linux
+	LIBS += -ltinfo -ldl
 endif
 
 PROGRAMS = hornet
@@ -109,7 +110,7 @@ all: $(PROGRAMS)
 
 hornet: $(OBJS) hornet.cc
 	$(E) "  LINK  " $@
-	$(Q) $(CXX) $(CXXFLAGS) $(OBJS) $(LDFLAGS) $(LIBS) -ltinfo -ldl -lz hornet.cc -o hornet
+	$(Q) $(CXX) $(CXXFLAGS) $(OBJS) $(LDFLAGS) $(LIBS) -lz hornet.cc -o hornet
 
 define INSTALL_EXEC
 	install -v $1 $(DESTDIR)$2/$1 || exit 1;