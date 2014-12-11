4d7c32cb46581d5f0f9a09f538f05e7c51f4ce83
Pekka Enberg <penberg@iki.fi>
2014-12-11 14:03:36 +0200

    Revert "java: Primitive class loading"
    This reverts commit dd71e99b2c4ed6a05c0cf02b029b3351e2fdcf94.
    
    It seems to make us try to free() a pointer that was not malloc'd.
    Probably one of the primitive class raw pointers gets converted to a
    shared_ptr and as that goes out of scope, we attempt to free statically
    allocated memory.
    
    Spotted breakage on both OS X and Linux.
    
    Signed-off-by: Pekka Enberg <penberg@iki.fi>

--- java/loader.cc
+++ java/loader.cc
@@ -36,20 +36,6 @@ std::shared_ptr<klass> loader::load_class(std::string class_name)
 {
     std::replace(class_name.begin(), class_name.end(), '.', '/');
 
-    auto klass = hornet::_jvm->lookup_class(class_name);
-    if (klass) {
-        return klass;
-    }
-    if (is_prim_type_name(class_name)) {
-        auto prim_klass = prim_sig_to_klass(class_name);
-        if (!prim_klass) {
-            hornet::throw_exception(java_lang_NoClassDefFoundError);
-            return nullptr;
-        }
-        klass = std::shared_ptr<hornet::klass>(prim_klass);
-        hornet::_jvm->register_class(klass);
-        return klass;
-    }
     if (is_array_type_name(class_name)) {
         auto elem_type_name = class_name.substr(1, std::string::npos);
         auto elem_type = load_class(elem_type_name);
@@ -61,16 +47,25 @@ std::shared_ptr<klass> loader::load_class(std::string class_name)
         hornet::_jvm->register_class(klass);
         return klass;
     }
+
+    auto klass = hornet::_jvm->lookup_class(class_name);
+
+    if (klass) {
+        return klass;
+    }
+
     klass = try_to_load_class(class_name);
 
     if (!klass) {
         hornet::throw_exception(java_lang_NoClassDefFoundError);
         return nullptr;
     }
+
     if (!klass->verify()) {
         throw_exception(java_lang_VerifyError);
         return nullptr;
     }
+
     return klass;
 }
 
